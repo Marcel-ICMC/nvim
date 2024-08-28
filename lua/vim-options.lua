b5acbe535c73ceeaa79346733917e5a5363314a6
Marcel-ICMC <marcelotoboni@gmail.com>
2024-08-27 21:34:58 -0300

    Adding fold config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,4 +3,12 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set relativenumber")
+
+vim.cmd("set foldenable")
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldtext = ""
+
 vim.g.mapleader = " "
