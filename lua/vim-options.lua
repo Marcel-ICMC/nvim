47cae9294ebddf06e03213be626aa7ed8b32f97e
Marcel-ICMC <marcelotoboni@gmail.com>
2025-10-01 19:50:55 -0300

    adding elixir ls

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,7 +8,7 @@ vim.cmd("set foldenable")
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldlevel = 99
+vim.cmd("set foldtext=getline(v:foldstart).'...'.trim(getline(v:foldend))")
 vim.opt.foldlevelstart = 1
-vim.opt.foldtext = ""
 
 vim.g.mapleader = " "
